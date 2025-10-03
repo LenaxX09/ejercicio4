0b71c594768abc3c3f4d2b3a7a3fd061ac955d73
LenaxX09 <acabanassantamaria@gmail.com>
2025-10-03 15:47:10 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/tsdmh252_csaap1e4/Saludo.java
@@ -0,0 +1,32 @@
+package mx.edu.tesoem.isc.tsdmh252_csaap1e4;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Saludo extends AppCompatActivity {
+
+    TextView lblp2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_saludo);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        lblp2 = findViewById(R.id.lblp2);
+        Bundle datos = getIntent().getExtras();
+        lblp2.setText("Bienvenido(a)"+ datos.getString("nombre")+ ", ya estas aprendiendo a programar android");
+
+
+    }
+}
